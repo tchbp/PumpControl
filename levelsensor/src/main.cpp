c45c4e47ec07a4beb3438cf922aecf712e81dd05
Banpot <tchbp0419@gmail.com>
2023-07-16 12:34:31 +0700

    modified:   levelsensor/platformio.ini 	modified:   levelsensor/src/main.cpp

--- levelsensor/src/main.cpp
+++ levelsensor/src/main.cpp
@@ -6,6 +6,7 @@
 #include <Ticker.h>
 #include <AsyncMqttClient.h>
 #include <TimeLib.h>
+#include <SoftwareSerial.h>
 
 #define trigPin D5
 #define echoPin D6
@@ -13,6 +14,8 @@
 #define SOUND_VELOCITY 0.034
   long duration;
   float distanceCm;
+  float temperature;
+  bool isUs100 = true;
 
 // Change the credentials below, so your ESP8266 connects to your router
 #define WIFI_SSID "BP-HOME"
@@ -27,11 +30,12 @@
 #define MQTT_PORT 14073
 #define MQTT_USER "poo"
 #define MQTT_PASSWORD "banpotbp"
-#define txtMyEsp "NodeMCU Water Level Sensor."
+#define txtMyEsp "NodeMCU Water Level Sensor With US-100."
 
 // MQTT Topics
 #define MQTT_PUB_Dist "MWL/dist"
 #define MQTT_PUB_Dist_LocDT "MWL/dist/locDT"
+#define MQTT_PUB_Temp "MWL/temp"
 
 AsyncWebServer server(80);
 
@@ -42,26 +46,58 @@ WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
 Ticker wifiReconnectTimer;
 
+SoftwareSerial us100(echoPin,trigPin);
+
 unsigned long previousMillis = 0;   // Stores last time temperature was published
 const long interval = 5000;        // Interval at which to publish sensor readings
 
+void us100_flush() {
+  while(us100.available())
+  us100.read();
+}
+
 void getDistance() 
 {
+  if (isUs100) {
+    us100_flush();
+    us100.write(0x55);
+    delay(100);
+    if (us100.available() < 2) {
+      Serial.println("Error read for US-100");
+      delay(500);
+      return;
+    }
+    distanceCm = (us100.read() * 256 + us100.read())/10;
 
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  
-  // Calculate the distance
-  distanceCm = duration * SOUND_VELOCITY/2;
+    us100_flush();
+    us100.write(0x50);
+    delay(100);
+    if (us100.available() < 1) {
+      delay(500);
+      return;
+    }
+    temperature = us100.read() - 45;
+    Serial.print("Temperature : ");
+    Serial.print(temperature);
+    Serial.println(" *C.");
 
+  } else {
+    // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(5);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    
+    // Calculate the distance
+    distanceCm = duration * SOUND_VELOCITY/2;
+
+  }
+  
   Serial.print("Distance (cm): ");
   Serial.println(distanceCm);
  
@@ -131,10 +167,13 @@ void onMqttPublish(uint16_t packetId) {
 
 void setup() {
   Serial.begin(9600);
-
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  
+  if (isUs100) {
+    us100.begin(9600);
+  } else {
+    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+    pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  }
+   
   wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
   wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
   
@@ -175,7 +214,14 @@ void loop() {
     // Publish an MQTT message
     uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_Dist, 1, true, String(distanceCm).c_str() );
     Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_Dist, packetIdPub1);
-    Serial.printf("Message: %.2f \n", distanceCm);
+    Serial.printf("Message: %.2f  cm\n", distanceCm);
+
+    if (isUs100) {
+      // Publish an MQTT message for Temperture.
+      uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_Temp, 1, true, String(temperature).c_str() );
+      Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_Temp, packetIdPub2);
+      Serial.printf("Message: %.2f *C\n", temperature);
+    }
 
   }
   
